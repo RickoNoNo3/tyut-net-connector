7696fc4a2b15223fb511c10496cb7a6f4dc1fc39
RickoNoNo3 <rickonono3@qq.com>
2023-11-09 22:54:10 +0800

    build.ps1

--- build.ps1
+++ build.ps1
@@ -1,36 +1,33 @@
+function Build {
+  param (
+    $GOARCH,
+    $GOOS,
+    $Name
+  )
+  mkdir ./build/$Name
+  SET CGO_ENABLED=0
+  SET GOARCH=$GOARCH
+  SET GOOS=$GOOS
+  if ($GOOS -eq 'windows') {
+    go build -o ./build/$Name/tyut-net-connector.exe .
+    cp ./startup.cmd ./build/$Name/
+  } else {
+    go build -o ./build/$Name/tyut-net-connector .
+    cp ./startup.sh ./build/$Name/
+    wsl -e chmod +x ./build/$Name/startup.sh
+  }
+  cp ./README.md ./build/$Name/
+  cd ./build
+  wsl -e zip -r ./tyut-net-connector-$Name.zip ./$Name
+  cd ../
+}
+
 rm -fo ./build
 mkdir ./build
-mkdir ./build/win_amd64
-mkdir ./build/osx_amd64
-mkdir ./build/linux_amd64
-mkdir ./build/linux_arm64
 
-SET CGO_ENABLED=0
-SET GOARCH=amd64
-SET GOOS=windows
-go build -o ./build/win_amd64/tyut-net-connector.exe .
-cp ./README.md ./build/win_amd64/
-cp ./startup.cmd ./build/win_amd64/
-wsl -e zip -r ./build/tyut-net-connector-win64.zip ./build/win_amd64
+Build -GOARCH amd64 -GOOS windows -Name win64
+Build -GOARCH amd64 -GOOS darwin -Name osx-intel64
+Build -GOARCH amd64 -GOOS linux -Name linux-amd64
+Build -GOARCH arm64 -GOOS linux -Name linux-arm64
 
-SET GOOS=darwin
-go build -o ./build/osx_amd64/tyut-net-connector .
-cp ./README.md ./build/osx_amd64/
-cp ./startup.sh ./build/osx_amd64/
-wsl -e chmod +x ./build/osx_amd64/startup.sh
-wsl -e zip -r ./build/tyut-net-connector-osx-intel64.zip ./build/osx_amd64
-
-SET GOOS=linux
-go build -o ./build/linux_amd64/tyut-net-connector .
-cp ./README.md ./build/linux_amd64/
-cp ./startup.sh ./build/linux_amd64/
-wsl -e chmod +x ./build/linux_amd64/startup.sh
-wsl -e zip -r ./build/tyut-net-connector-linux-amd64.zip ./build/linux_amd64
-
-SET GOARCH=arm64
-SET GOOS=linux
-go build -o ./build/linux_arm64/tyut-net-connector .
-cp ./README.md ./build/linux_arm64/
-cp ./startup.sh ./build/linux_arm64/
-wsl -e chmod +x ./build/linux_arm64/startup.sh
-wsl -e zip -r ./build/tyut-net-connector-linux-arm64.zip ./build/linux_arm64
+echo BUILD DONE
